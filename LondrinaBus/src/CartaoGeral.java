ef161c82a3eb5c27062c9753cd6653efc58480be
marcotulio <marcotulio.barros@uel.br>
2023-09-18 22:39:47 -0300

    Simplificação de classas, banco e testes de controller

--- LondrinaBus/src/CartaoGeral.java
+++ LondrinaBus/src/CartaoGeral.java
@@ -2,66 +2,66 @@ import java.util.Scanner;
 import java.util.UUID;
 
 public class CartaoGeral {
-    private String codigoNFC;
-    private int numUnico;
-    private int saldoCredito;
-    private int quantidadeCreditos;
-    private double valorCredito;
+    protected String codigoNFC;
+    protected int numUnico;
+    protected int saldoCredito;
+    protected double valorCredito;
+    protected boolean isEstudante;
 
-    public CartaoGeral(int numUnico, int saldoCredito, double valorCredito) {
+    public CartaoGeral() {}
+
+    // usar quando for de fato criar um novo usuario
+    public CartaoGeral(int numUnico) {
         this.codigoNFC = UUID.randomUUID().toString(); //Gerar um valor randon para o NFC do cartão
-        this.numUnico = numUnico; //Número do cartão
-        this.saldoCredito = saldoCredito; //Saldo de Crédito
-        this.quantidadeCreditos = 0; //Quantidade inicial de créditos na conta
-        this.valorCredito = valorCredito; //Preço do crédito
+         this.numUnico = numUnico; //Número do cartão
+         this.saldoCredito = 0; //Saldo de Crédito
+         this.valorCredito = 4.80; //Preço do crédito
+         this.isEstudante = false;
     }
 
     //Compra de Crédito (Provisório)
-    public void comprarCreditos() {
+    public void addCreditosGeral() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite a quantidade de créditos desejada: ");
         int quantidadeDesejada = scanner.nextInt();
-        scanner.nextLine(); 
+        scanner.nextLine();
 
         if (quantidadeDesejada > 0) {
-            quantidadeCreditos += quantidadeDesejada;
             double valorTotal = quantidadeDesejada * valorCredito;
-            saldoCredito += valorTotal;
+            saldoCredito += quantidadeDesejada;
             System.out.println(quantidadeDesejada + " crédito(s) comprado(s) com sucesso por um valor total de R$ " + valorTotal);
+            System.out.println("Saldo atual: " + saldoCredito);
         } else {
             System.out.println("A quantidade desejada de créditos deve ser maior que zero.");
         }
     }
 
-    public String getCodigoNFC() {
-        return codigoNFC;
+    public void setCodigoNFC(String cod) { this.codigoNFC = cod; }
+    public String getCodigoNFC() { return codigoNFC; }
+
+    public void setNumUnico(int num) { this.numUnico = num; }
+    public int getNumUnico() { return numUnico; }
+
+    public void setSaldoCredito(int s) { this.saldoCredito = s; }
+    public int getSaldoCredito() { return saldoCredito; }
+
+    public void setValorCredito(double v) { this.valorCredito = v; }
+    public double getValorCredito() { return valorCredito; }
+
+    public void setEstudante( boolean b) { this.isEstudante = b; }
+    public boolean getEstudante() { return  this.isEstudante; }
+
+    public void defineAsEstudante() {
+        this.isEstudante = true;
+        this.valorCredito = 2.40;
     }
 
-    public int getNumUnico() {
-        return numUnico;
+    public void printStatusCartao() {
+        if (this.isEstudante) {
+            System.out.println("CARTAO ESTUDANTE");
+        } else {
+            System.out.println("CARTAO GERAL");
+        }
     }
 
-    public int getSaldoCredito() {
-        return saldoCredito;
-    }
-
-    public int getQuantidadeCreditos() {
-        return quantidadeCreditos;
-    }
-
-    public double getValorCredito() {
-        return valorCredito;
-    }
-
-    public static void main(String[] args) {
-        //Menu com as informações do cartão
-        CartaoGeral cartao = new CartaoGeral(12345, 0, 4.80);
-        System.out.println("Código NFC: " + cartao.getCodigoNFC()); 
-        System.out.println("Número Único: " + cartao.getNumUnico());
-        System.out.println("Saldo de Crédito: " + cartao.getSaldoCredito());
-        System.out.println("Valor de Cada Crédito: " + cartao.getValorCredito());
-
-        cartao.comprarCreditos(); // Solicita a quantidade de créditos desejada
-        System.out.println("Quantidade de Créditos após compra: " + cartao.getQuantidadeCreditos());
-    }
 }
--- LondrinaBus/src/GerenciadorAppOnibus.java
+++ LondrinaBus/src/GerenciadorAppOnibus.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.function.Function;
 import java.util.logging.Level;
@@ -6,31 +7,31 @@ import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
 public class GerenciadorAppOnibus {
-    private ArrayList<Usuario> usuarios;
+    private ArrayList<CartaoGeral> cartoes = new ArrayList<>();
+    private ArrayList<Usuario> usuarios = new ArrayList<>();
     private DatabaseManagerApp databaseManager = new DatabaseManagerApp();
     PreparedStatement preparedStatement;
-    public void queryUsuarios() {
+    public void queryUsuario() {
 
         databaseManager.connect();
         Connection connection = databaseManager.getConnection();
 
         try {
             preparedStatement = connection.prepareStatement(
-                    "SELECT * from pacientes"
+                    "SELECT * from usuario"
             );
             ResultSet resultSet = preparedStatement.executeQuery();
 
             {
                 while (resultSet.next()) {
-                    Registro registro = new Registro();
-                    CartaoGeral cartaoGeral = new CartaoGeral();
-                    registro.setNome(resultSet.getString("Login"));
-                    registro.setSenha(resultSet.getString("Senha"));
-                    registro.setNome(resultSet.getString("Nome"));
-                    registro.setCep(resultSet.getString("Cep"));
-                    registro.setCpf(resultSet.getString("Cpf"));
-                    registro.setEndereco(resultSet.getString("Endereco"));
-                    Usuario user = new Usuario(registro, cartaoGeral);
+                    Usuario user = new Usuario();
+                    user.setCpf(resultSet.getString("cpf"));
+                    user.setLogin(resultSet.getString("login"));
+                    user.setSenha(resultSet.getString("senha"));
+                    user.setNome(resultSet.getString("nome"));
+                    user.setCep(resultSet.getString("cep"));
+                    user.setEndereco(resultSet.getString("endereco"));
+                    user.setNumUnicoCartao(resultSet.getInt("numUnicoCartao"));
                     usuarios.add(user);
                 }
             }
@@ -38,5 +39,37 @@ public class GerenciadorAppOnibus {
         } catch (SQLException ex) {
             Logger.getLogger(GerenciadorAppOnibus.class.getName()).log(Level.SEVERE, null, ex);
         }
+        for (Usuario u : usuarios)
+            System.out.printf("Usuário com dados: %s %s %d\n", u.getCpf(), u.getNome(), u.getNumUnicoCartao());
+    }
+
+    public void queryCartaoGeral() {
+
+        databaseManager.connect();
+        Connection connection = databaseManager.getConnection();
+
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "SELECT * from cartaogeral"
+            );
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            {
+                while (resultSet.next()) {
+                    CartaoGeral cartaoGeral = new CartaoGeral();
+                    cartaoGeral.setNumUnico(resultSet.getInt("numUnico"));
+                    cartaoGeral.setCodigoNFC(resultSet.getString("codigoNFC"));
+                    cartaoGeral.setEstudante(resultSet.getBoolean("isEstudante"));
+                    cartaoGeral.setSaldoCredito(resultSet.getInt("saldoCredito"));
+                    cartaoGeral.setValorCredito(resultSet.getDouble("valorCredito"));
+                    cartoes.add(cartaoGeral);
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(GerenciadorAppOnibus.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        for  (CartaoGeral cg : cartoes)
+            System.out.println("CartãoGeral id: " + cg.numUnico);
     }
 }
